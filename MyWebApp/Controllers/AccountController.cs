f56f7faff816c703d0a9f31bad5eb1e49ee80f8e
priyanka0523 <priyankalakkireddy533@gmail.com>
2023-04-16 01:08:07 -0400

    Add project files.

---
+++ MyWebApp/Controllers/AccountController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController, Produces("application/json")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult> Register([FromBody] UserRegistrationRequest request)
+        {
+            if (request == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var identityUser = new IdentityUser { UserName = request.UserName, Email = request.Email };
+            var result = await _userManager.CreateAsync(identityUser, request.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginCredentials credentials)
+        {
+            if (credentials == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var identityUser = await _userManager.FindByNameAsync(credentials.UserName);
+            if (identityUser == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = _userManager.PasswordHasher.VerifyHashedPassword(identityUser, identityUser.PasswordHash, credentials.Password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Email, identityUser.Email),
+                new(ClaimTypes.Name, identityUser.UserName)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+            return Ok();
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+    }
+
+    public class UserRegistrationRequest
+    {
+        [Required] public string UserName { get; set; }
+        [Required] public string Password { get; set; }
+        [Required] public string Email { get; set; }
+    }
+
+    public class LoginCredentials
+    {
+        [Required] public string UserName { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
